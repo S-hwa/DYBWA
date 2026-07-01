a3efddf45d8256e2a5b50faffba4378874a33357
S-hwa <ramosjoshua0987@gmail.com>
2026-07-01 16:37:22 +0800

    Create GAG2LOADSKIP.lua

---
+++ GAG2LOADSKIP.lua
@@ -0,0 +1,27 @@
+local function performClick()
+    VirtualInputManager:SendMouseButtonEvent(0, 0, 0, true, game, 1)
+    task.wait(0.05)
+    VirtualInputManager:SendMouseButtonEvent(0, 0, 0, false, game, 1)
+end
+
+local Players = game:GetService("Players")
+local VirtualInputManager = game:GetService("VirtualInputManager")
+
+local player = Players.LocalPlayer
+
+local clickScreen = task.spawn(function()
+    while true do
+        performClick()
+        task.wait(0.25)
+    end
+end)
+
+local function waitForLoadingScreen()
+	while not player:GetAttribute("LoadingScreenDone") do
+		task.wait(0.25)
+	end
+
+   task.cancel(clickScreen)
+   print("✅ Loading screen skipped!")
+	return true
+end
